36c080713d44eb0477f80d3d5fa20f2445559e34
Jay Campbell <jay.campbell93@outlook.com>
2026-01-21 22:48:36 -0800

    Initial commit: Astro portfolio setup

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from "@tailwindcss/vite";
+import astroExpressiveCode from "astro-expressive-code";
+import { defineConfig } from 'astro/config';
+import config from "./src/config/site.config.json";
+
+// https://astro.build/config
+export default defineConfig({
+  site: config.baseUrl ? config.baseUrl : "http://examplesite.com",
+  base: config.basePath ? config.basePath : "/",
+  trailingSlash: config.trailingSlash ? "always" : "ignore",
+  markdown: {
+    remarkPlugins: [],
+  },
+  integrations: [
+    astroExpressiveCode({
+      themes: ["andromeeda"],
+    }),
+    mdx(),
+    sitemap(),
+    react()
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
